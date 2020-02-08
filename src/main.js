0d484e1f3eb4d7ae882b14711fb8afac657008d0
zzj <zhengzijian56@gmail.com>
2020-02-08 08:46:08 +0000

    init

---
+++ src/main.js
@@ -0,0 +1,86 @@
+let html = document.querySelector("#html")
+let style = document.querySelector("#style")
+let string = `/* 你好我是zzj
+ * 我要准备一个div
+ */
+#div1{
+    border: 1px solid red;
+    width: 200px;
+    height: 200px;
+}
+/* 
+ * 接下来我把 div 变成一个八卦图
+ * 注意看好了
+ * 首先把 div 变成一个圆
+ */
+#div1{
+    border-radius: 50%;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    border: none;
+}
+/* 八卦是阴阳形成的
+ * 一黑一白
+ */
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/* 再加两个神秘的小球 */
+#div1::before{
+    width: 100px;
+    height: 100px;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #000;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#div1::after{
+    width: 100px;
+    height: 100px;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #fff;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);
+}
+`
+let string2 = ""
+let n = 0
+
+let step = () => {
+  setTimeout(() => {
+    if (n < string.length) {
+      if (string[n] === "\n") {
+        string2 += "<br>"
+      } else if (string[n] === " ") {
+        string2 += "&nbsp"
+      } else {
+        string2 += string[n]
+      }
+      html.innerHTML = string2
+      style.innerHTML = string.substring(0, n)
+      n += 1
+      window.scrollTo(0, 9999)
+      html.scrollTo(0, 99999)
+      step()
+    }
+  }, 0)
+}
+step()
+
+let step_while = () => {
+  setTimeout(() => {
+    if (n < string.length) {
+      //   if (string[n] === "\n") {
+      //     string2 += "<br>"
+      //   } else {
+      //     string2 += string[n]
+      //   }
+      string2 += string[n] === "\n" ? "<br>" : string[n]
+      demo.innerHTML = string2
+      n += 1
+      step_while()
+    }
+  }, 100)
+}
